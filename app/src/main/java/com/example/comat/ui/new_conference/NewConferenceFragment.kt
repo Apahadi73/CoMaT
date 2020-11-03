9c65091796142baa71db3b48f9d213b561a16b4d
Amir <apahadi73@gmail.com>
2020-11-03 03:47:45 -0600

    working on new conference fragment

--- app/src/main/java/com/example/comat/ui/new_conference/NewConferenceFragment.kt
+++ app/src/main/java/com/example/comat/ui/new_conference/NewConferenceFragment.kt
@@ -1,31 +1,142 @@
 package com.example.comat.ui.new_conference
 
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.basgeekball.awesomevalidation.AwesomeValidation
+import com.basgeekball.awesomevalidation.ValidationStyle
+import com.basgeekball.awesomevalidation.utility.RegexTemplate
 import com.example.comat.R
+import com.example.comat.databinding.FragmentNewConferenceBinding
+import java.util.*
 
 class NewConferenceFragment : Fragment() {
 
     private lateinit var newConferenceViewModel: NewConferenceViewModel
+    private var SELECT_PHOTO: Int = 1
+    private lateinit var binding: FragmentNewConferenceBinding
+    private lateinit var imageURI: Uri
+    private var dateString: String? = null
+    private var isLogoPicked: Boolean = false
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         newConferenceViewModel =
-                ViewModelProvider(this).get(NewConferenceViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_new_conference, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        newConferenceViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+            ViewModelProvider(this).get(NewConferenceViewModel::class.java)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_new_conference, container, false)
+//        picks an image from user
+        binding.pickImage.setOnClickListener {
+            val intent: Intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, SELECT_PHOTO)
+        }
+
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        binding.pickDateBtn.setOnClickListener {
+            context?.let { it1 ->
+                DatePickerDialog(
+                    it1,
+                    DatePickerDialog.OnDateSetListener { view, mYear, mMonth, mDay ->
+                        dateString = "$mDay/$mMonth/$mYear"
+                        binding.showDate.text = "Date Picked: $dateString"
+                    },
+                    year,
+                    month,
+                    day
+                )
+            }?.show()
+        }
+
+//        validation
+        val validator = AwesomeValidation(ValidationStyle.BASIC)
+        validator.addValidation(
+            activity,
+            R.id.conference_name,
+            ".{4,}",
+            R.string.invalid_conference_name
+        )
+        validator.addValidation(
+            activity,
+            R.id.conference_description,
+            ".{10,}",
+            R.string.invalid_conference_description
+        )
+        validator.addValidation(
+            activity,
+            R.id.conf_schedule,
+            RegexTemplate.NOT_EMPTY,
+            R.string.invalid_conference_schedule
+        )
+        validator.addValidation(
+            activity,
+            R.id.conf_speakers,
+            RegexTemplate.NOT_EMPTY,
+            R.string.invalid_conference_speakers
+        )
+        validator.addValidation(
+            activity,
+            R.id.conf_venue,
+            RegexTemplate.NOT_EMPTY,
+            R.string.invalid_conference_venue
+        )
+        binding.createConf.setOnClickListener {
+            Log.d("reached", "button clicked")
+            if (validator.validate()) {
+                if (!isLogoPicked) {
+                    makeText(context, "Please pick a logo", LENGTH_LONG)
+                } else if (dateString == null) {
+                    makeText(context, "Please pick a date", LENGTH_LONG)
+                } else {
+                    activity?.applicationContext?.let {
+                        newConferenceViewModel.createNewConference(
+                            it,
+                            imageURI,
+                            binding.conferenceName.text.toString(),
+                            binding.conferenceDescription.text.toString(),
+                            binding.showDate.text.toString(),
+                            binding.confSchedule.text.toString(),
+                            binding.confSpeakers.text.toString(),
+                            binding.confVenue.text.toString(),
+                        )
+                    }
+                }
+            }
+        }
+
+        return binding.root
+    }
+
+
+    //    handles image picker result
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == SELECT_PHOTO) {
+            // handle chosen image
+            if (data != null) {
+                imageURI = data.data!!
+                binding.confLogo.setImageURI(imageURI)
+                isLogoPicked = true
+            }
+        }
     }
 }
--- app/src/main/java/com/example/comat/ui/new_conference/NewConferenceViewModel.kt
+++ app/src/main/java/com/example/comat/ui/new_conference/NewConferenceViewModel.kt
@@ -1,13 +1,53 @@
 package com.example.comat.ui.new_conference
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.example.comat.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class NewConferenceViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is new conference Fragment"
+    fun createNewConference(context: Context, imageUri: Uri,name:String,description:String,date:String,schedule:String,speakers:String,venue:String): Unit {
+        Log.d("reached", "reached")
+        val user = FirebaseAuth.getInstance().currentUser
+        val databaseReference = FirebaseDatabase.getInstance().reference
+        val storageReference = FirebaseStorage.getInstance().reference;
+        val imageStorageBucketRef: StorageReference = storageReference.child("images/${user?.uid}")
+//        puts image into the firebase storage, fetches image download url and uploads the user profile to the backend
+        imageStorageBucketRef.putFile(imageUri)
+            .addOnSuccessListener { taskSnapshot -> // Get a URL to the uploaded content
+                // Get a URL to the uploaded content
+                val downloadTask: Task<Uri> = taskSnapshot.storage.downloadUrl
+                downloadTask.addOnSuccessListener {
+                    val imageDownloadUrl = it.toString()
+                    Log.d("response", "downloadUrl:$imageDownloadUrl")
+                    val userDb = user?.uid?.let { it1 -> databaseReference.child("users").child(it1) }
+                    if (userDb != null) {
+                        userDb.child("name").setValue(name)
+                        userDb.child("description").setValue(description)
+                        userDb.child("date").setValue(date)
+                        userDb.child("schedule").setValue(schedule)
+                        userDb.child("speakers").setValue(speakers)
+                        userDb.child("venue").setValue(venue)
+                        userDb.child("logoUrl").setValue(imageDownloadUrl)
+                        NavController(context).navigate(R.id.action_nav_new_conference_to_nav_conferences)
+                    }
+                }
+                Log.d("response", "Successfully uploaded the data to firebase")
+            }
+            .addOnFailureListener {
+                Log.d("response", "Unsuccessful to upload the image to firebase")
+            }
     }
-    val text: LiveData<String> = _text
 }
